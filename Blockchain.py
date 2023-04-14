785d4711b8152e0a9c54e5feeffd25ae8c13cf07
Robyn Edsitty <98797776+robynmyra@users.noreply.github.com>
2023-04-14 15:04:02 -0700

    more progress on the input parsing

--- Blockchain.py
+++ Blockchain.py
@@ -23,34 +23,73 @@ def init():
 def verify():
     print("verify")      
     
-    
-if (len(sys.argv)) > 1:
-    arg1 = sys.argv[1]
+input = (sys.argv)
+
+if (len(input)) > 1:
 
     # if 2nd argument is "add" ------------- 
-    if arg1 == "add":
+    if input[1] == "add":
         add() 
+    
+        # if 3rd argument is "-c"
+        if input[2] == "-c":
+            caseID = input[3]
+            print(caseID)
+    
+        # loop for item_number input
+        for i in range(4, len(input), 2):
+            if input[i] == "-i":
+                itemID = input[i+1]
+                print(itemID)
+        
         
     # if 2nd argument is "checkout" -------------     
-    elif arg1 == "checkout":
+    elif input[1] == "checkout":
         checkout()
+         
+        # loop for item_number input
+        for i in range(4, len(input), 2):
+            if input[i] == "-i":
+                itemID = input[i+1]
+                print(itemID)
         
     # if 2nd argument is "checkin" -------------     
-    elif arg1 == "checkin":
+    elif input[1] == "checkin":
         checkin()
+                
+        # loop for item_number input
+        for i in range(4, len(input), 2):
+            if input[i] == "-i":
+                itemID = input[i+1]
+                print(itemID)
         
     # if 2nd argument is "log" -------------     
-    elif arg1 == "log":
-        log()    
+    if input[1] == "log":
+        log()   
+        
+        # loop for log input 
+        for i in range(1, len(input)):
+            
+            if input[i] == "-r":
+                print("-r")
+                
+            elif input[i] == "-n":
+                print("-n")
+                
+            elif input[i] == "-c":
+                print("-c")
+                
+            elif input[i] == "-i":
+                print("-i")
       
     # if 2nd argument is "remove" -------------     
-    elif arg1 == "remove":
+    elif input[1] == "remove":
         remove()       
         
     # if 2nd argument is "init" ------------- 
-    elif arg1 == "init":
+    elif input[1] == "init":
         init()       
         
     # if 2nd argument is "verify" -------------     
-    elif arg1 == "verify":
-        verify()    
+    elif input[1] == "verify":
+        verify() 
